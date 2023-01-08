8fcd7614ff9b7ee34f35bf76e0ce54e70f1d9834
Dezkiel <dezkiel@gmail.com>
2023-01-08 18:40:06 +0100

    Added color picker and two methods of drawing: on click and drag

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const btn10 = document.getElementById('grid-10x10');
 const btn30 = document.getElementById('grid-30x30');
 const btn50 = document.getElementById('grid-50x50');
 const btnClear = document.getElementById('grid-clear');
+const btnDrag = document.getElementById('mouseover');
+const btnClick = document.getElementById('mouseclick');
+const colorPicker = document.getElementById('colorpicker');
 let gridHeight = 10;
 let gridWidth = 10;
 let gridSize = gridHeight * gridWidth;
-let cellColor = 'red';
+let cellColor = colorPicker.value;
+let isMouseoverActive = 'click';
 
 function setGridContainerStyles(height, width)
 {
@@ -27,18 +31,42 @@ function createGrid(size)
 }
 
 function changeCellColor(color, gridSize) 
-{
+{    
     for(i = 0; i < gridSize; i++)
     {
         const cell = document.getElementById(`cell${i}`);
-        cell.addEventListener('mouseover', () =>{
+        cell.addEventListener(`${isMouseoverActive}`, () =>{
             cell.style.backgroundColor = `${color}`;
         });
-    }
+    }   
 }
+
+colorPicker.addEventListener('change', () =>{
+    cellColor = colorPicker.value;
+    changeCellColor(cellColor, gridSize);
+})
 
 setGridContainerStyles(gridHeight, gridWidth);
 createGrid(gridSize);
+changeCellColor(cellColor, gridSize);
+
+btnDrag.addEventListener('click', () =>{ 
+    isMouseoverActive = 'mouseover';
+    gridContainer.replaceChildren();
+
+    setGridContainerStyles(gridHeight, gridWidth);
+    createGrid(gridSize);
+    changeCellColor(cellColor, gridSize);
+});
+
+btnClick.addEventListener('click', () =>{
+    isMouseoverActive = 'click';
+    gridContainer.replaceChildren();
+
+    setGridContainerStyles(gridHeight, gridWidth);
+    createGrid(gridSize);
+    changeCellColor(cellColor, gridSize);
+});
 
 btn10.addEventListener('click', () =>
 {
